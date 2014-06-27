422d719e2023458f8e1b3107a9f8282fa53e0c18
Heiko Braun <ike.braun@googlemail.com>
2014-06-27 10:38:14 +0200

    show default search screen

--- redhat-support-plugin-war/src/main/webapp/js/search.js
+++ redhat-support-plugin-war/src/main/webapp/js/search.js
@@ -1,5 +1,8 @@
 angular.module('RedhatAccess.customSearch', [ 'RedhatAccess.search'])
-		.controller('search', ['$scope', 'securityService',
-			function($scope, securityService) {
-				
-			} ]);
+        .controller('search', ['$scope', '$location', 'securityService',
+            function($scope, $location, securityService) {
+                // default location
+                $scope.init = function () {
+                     $location.path('search');
+                };
+            } ]);
