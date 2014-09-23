0881d537ec4f3f948a617565038d8df89a6049b4
Heiko Braun <ike.braun@googlemail.com>
2014-09-23 12:19:58 +0200

    Fix HAL-476: Broken layout in CSP app

--- webapp/src/main/webapp/csp/js/custom-cases.js
+++ webapp/src/main/webapp/csp/js/custom-cases.js
@@ -1,20 +1,41 @@
+
+angular.module('RedhatAccess.common').directive('showtab',
+    function () {
+        return {
+            link: function (scope, element, attrs) {
+                element.click(function(e) {
+                    e.preventDefault();
+                    $(element).tab('show');
+                });
+            }
+        };
+    });
+
 angular.module('RedhatAccess.customCaseView', ['RedhatAccess.cases'])
-.controller('customCase', ['$scope', '$location', 'securityService', 'NEW_DEFAULTS', function($scope, $location, securityService, NEW_DEFAULTS) {
-	NEW_DEFAULTS.product = "Red Hat JBoss Enterprise Application Platform";
-	NEW_DEFAULTS.version = "6.2.0";
-	 $scope.selected = 'open-case';
-	 $scope.openCaseClick = function(){
-		 $location.path('case/new');
-	 };
-	 $scope.modifyCaseClick = function(){
-		 $location.path('case/compact');
-	 };
-	 $scope.init = function () {
-		 securityService.validateLogin(true);
-		 if($location.$$path == '/case/compact'){
-			 $scope.selected = 'modify-case';
-		 } else{
-			 $location.path('case/new');
-		 }
-	 };
-}]);
+        .controller('customCase', ['$scope', '$location', 'securityService', 'NEW_DEFAULTS', function($scope, $location, securityService, NEW_DEFAULTS) {
+            NEW_DEFAULTS.product = "Red Hat JBoss Enterprise Application Platform";
+            NEW_DEFAULTS.version = "6.2.0";
+            $scope.selected = 'search';
+            $scope.openSearchClick = function(){
+                $location.path('search');
+            };
+            $scope.openCaseClick = function(){
+                $location.path('case/new');
+            };
+            $scope.modifyCaseClick = function(){
+                $location.path('case/list');
+            };
+            $scope.init = function () {
+                securityService.validateLogin(true);
+                if($location.$$path == '/case/list'){
+                    $scope.selected = 'modify-case';
+                }
+                else if($location.$$path == '/case/new'){
+                    $scope.selected = 'open-case';
+                }
+                else{
+                    $scope.selected = 'search';
+                    $location.path('search');
+                }
+            };
+        }]);
