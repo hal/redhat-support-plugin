52fe5e4d2481a73ff90f004b4f138b2b4405215f
Heiko Braun <ike.braun@googlemail.com>
2014-10-27 14:30:59 +0100

    Update to 0.9.34

---
+++ README.md
@@ -0,0 +1,9 @@
+## build it
+
+`mvn clean install`
+
+## test it
+
+`npm install http-server -g`
+
+`./server.sh`
--- webapp/src/main/webapp/csp/js/custom-cases.js
+++ webapp/src/main/webapp/csp/js/custom-cases.js
@@ -1,17 +1,4 @@
 
-/*angular.module('RedhatAccess.common').directive('showtab',
-    function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.click(function(e) {
-                    e.preventDefault();
-                    $(element).tab('show');
-                });
-            }
-        };
-    });*/
-
-
 angular.module('RedhatAccess.common').run(
         ['SECURITY_CONFIG', function (SECURITY_CONFIG) { SECURITY_CONFIG.forceLogin = true; }
 ]);
@@ -21,15 +8,7 @@ angular.module('RedhatAccess.customCaseView', ['RedhatAccess.cases'])
             NEW_DEFAULTS.product = "Red Hat JBoss Enterprise Application Platform";
             NEW_DEFAULTS.version = "6.2.0";
             $scope.selected = 'search';
-            /*$scope.openSearchClick = function(){
-                $location.path('search');
-            };
-            $scope.openCaseClick = function(){
-                $location.path('case/new');
-            };
-            $scope.modifyCaseClick = function(){
-                $location.path('case/list');
-            };*/
+
             $scope.init = function () {
                 securityService.validateLogin(true);
                 if($location.$$path == '/case/list'){
